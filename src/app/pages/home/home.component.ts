8842e65333d84b20dd18d5abe1143b37ef9da424
AOcampo93 <54147798+ArturoOcampo93@users.noreply.github.com>
2022-05-08 22:21:53 -0500

    Modal de Inicio y Legales
    + Modal de inicio con el trailer de la pelicula
    +Legales (Aviso, Terminos y Bases)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {this.openPopup()}
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 
+displayStyle = "none";
 
+openPopup() {
+    this.displayStyle = "block";
 }
-
+closePopup() {
+    this.displayStyle = "none";
+}
+}
